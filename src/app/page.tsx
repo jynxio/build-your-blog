f7a756c7a1bb4dbca9f2ea4d1d652d4897ba5072
jynxio <jinxiaomatrix@gmail.com>
2024-06-28 01:00:45 +0800

    Adding Syntax Highlighting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
+import CodeSnippet from "@/component/code-snippet";
 import { reqPost } from "@/helper/post";
 import { MDXRemote } from "next-mdx-remote/rsc";
 
 async function Page() {
     const post = await reqPost("/post/build-interative-blog/index.md");
+    const components = { pre: CodeSnippet };
 
-    return <MDXRemote source={post.content} />;
+    return <MDXRemote source={post.content} components={components} />;
 }
 
 export default Page;
