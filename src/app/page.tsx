5d0877485ba19e1a71512d7e302a40526b7cc76e
jynxio <jinxiaomatrix@gmail.com>
2024-06-28 00:55:00 +0800

    Rendering Post Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
-function Page() {
-    return <h1>What would you like to know?</h1>;
+import { reqPost } from "@/helper/post";
+
+async function Page() {
+    const post = await reqPost("/post/build-interative-blog/index.md");
+
+    return <article>{post.content}</article>;
 }
 
 export default Page;
